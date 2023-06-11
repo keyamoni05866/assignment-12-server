f19065152abbdfe486fa8055e99a19b4bb0b7082
keyamoni05866 <keya05866@gmail.com>
2023-06-11 23:42:32 +0600

    popular instructor get done

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 // mongodb related operations start from here
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nn0l6mi.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,95 +28,109 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
     // collections
     const usersCollection = client.db("TuneCamp").collection("users");
     const classesCollection = client.db("TuneCamp").collection("classes");
 
-
-
     // users apis
     // user post operations
-    app.post('/users', async(req, res) =>{
-        const user = req.body;
-        const query = {email: user.email};
-        const existingUser = await usersCollection.findOne(query);
-        if(existingUser){
-          return res.send({message: 'user already exist'})
-        }
-        const result =await usersCollection.insertOne(user);
-        res.send(result);
-    })
-    // user get 
-    app.get('/users', async(req, res) =>{
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // user get
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     // instructors apis
-     app.get('/users/instructor', async(req, res) =>{
-      const query = { role: 'instructor' };
+    app.get("/users/instructor", async (req, res) => {
+      const query = { role: "instructor" };
       const cursor = usersCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+      // get 6 instructor
+     app.get('/users/6instructor', async(req, res) =>{
+      const query = {role: "instructor"};
+      const result = await usersCollection.find(query).limit(6).toArray();
       res.send(result)
      })
-
-
 
     // user patch operation
     // admin
-    app.patch('/users/admin/:id', async(req, res) =>{
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc ={
-        $set:{
-          role: 'admin'
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
         },
       };
-      const result = await usersCollection.updateOne(filter, updateDoc)
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-    } )
+    });
     // instructor
-    app.patch('/users/instructor/:id', async(req, res) =>{
+    app.patch("/users/instructor/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc ={
-        $set:{
-          role: 'instructor'
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
         },
       };
-      const result = await usersCollection.updateOne(filter, updateDoc)
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-    } )
+    });
 
     // Add Classes related apis
-     app.post('/addClasses', async(req, res) =>{
-         const classes = req.body;
-         const result = await classesCollection.insertOne(classes);
-         res.send(result) 
-     })
+    app.post("/addClasses", async (req, res) => {
+      const classes = req.body;
+      const result = await classesCollection.insertOne(classes);
+      res.send(result);
+    });
     //  get classes apis
-    app.get('/addClasses', async(req, res) =>{
+    app.get("/addClasses", async (req, res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     // classes approved operations
-    app.patch('/addClasses/:id', async(req, res) =>{
+    app.patch("/addClasses/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id)
-      const filter = {_id: new ObjectId(id)}
-      
-      const updateDoc={
-        $set:{
-          status: 'approved'
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved",
         },
       };
       const result = await classesCollection.updateOne(filter, updateDoc);
-      res.send(result)
-    })
-    
+      res.send(result);
+    });
+        // get approved classes
+    app.get("/addClasses/approved", async (req, res) => {
+      const query = { status: "approved" };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    // // get just 6 class for home page
+    // app.get("/", async(req, res) =>{
+    //   const query = { status: "approved" };
+    //   const result = await classesCollection.find(query).limit(6).toArray();
+    //   res.send(result)
+    // })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -125,18 +138,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("tuneCamp is running");
+});
 
-
-
-
-
-
-
-
-app.get('/', (req, res) =>{
-    res.send('tuneCamp is running')
-})
-
-app.listen(port, () =>{
-    console.log(`TuneCamp is running on${port}`)
-})
+app.listen(port, () => {
+  console.log(`TuneCamp is running on${port}`);
+});
