4b15801b3c570f109e5d82a9cf9f3be4b8317c8e
keyamoni05866 <keya05866@gmail.com>
2023-06-13 22:04:53 +0600

    select classes delete opreration done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -8,6 +9,25 @@ require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// jwt middleware
+const verifyJWT = (req, res, next)=>{
+         const authorization = req.headers.authorization;
+         if(!authorization){
+          return res.status(401).send({error: true, message: 'unauthorized access'});
+         }
+         const token = authorization.split(' ')[1];
+
+         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,  (err, decoded) =>{
+          if(err){
+            return res.status(401).send({error: true,  message: 'unauthorized access'})
+          }
+          req.decoded = decoded;
+          next();
+         })
+}
+
+
 
 // mongodb related operations start from here
 
@@ -34,6 +54,19 @@ async function run() {
     // collections
     const usersCollection = client.db("TuneCamp").collection("users");
     const classesCollection = client.db("TuneCamp").collection("classes");
+    const selectClassesCollection = client.db("TuneCamp").collection("selectClasses");
+
+  //  jwt post
+  app.post('/jwt', (req, res)=>{
+         const user = req.body;
+         const token  = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'}) 
+         res.send({token})
+  })
+
+
+
+
+
 
     // users apis
     // user post operations
@@ -100,7 +133,7 @@ async function run() {
       res.send(result);
     });
     //  get classes apis
-    app.get("/addClasses", async (req, res) => {
+    app.get("/addClasses",  async (req, res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
@@ -125,12 +158,32 @@ async function run() {
       res.send(result);
     });
 
-    // // get just 6 class for home page
-    // app.get("/", async(req, res) =>{
-    //   const query = { status: "approved" };
-    //   const result = await classesCollection.find(query).limit(6).toArray();
-    //   res.send(result)
-    // })
+    // select class related apis
+    app.post('/selectClasses', async(req, res)=>{
+      const classes = req.body;
+      console.log(classes);
+      const result = await selectClassesCollection.insertOne(classes);
+      res.send(result);
+    })
+
+    app.get('/selectClasses', async(req, res) =>{
+      const email = req.query.email;
+      // if(!email){
+      //   res.send([])
+      // }
+      const query = { email: email };
+      const result = await selectClassesCollection.find(query).toArray();
+      res.send(result)
+    })
+    app.delete('/selectClasses/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)};
+      const result = await selectClassesCollection.deleteOne(query);
+      res.send(result)
+    })
+
+  
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
